525cd50aa0e97a526e0ac5d563542fc03592b9e5
Anand Gupta <anand.bdk@gmail.com>
2012-04-27 01:55:35 -0700

    use of the first line of python

---
+++ src/fsfuse.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# the above line is written for three reason
+# 1. to specify that the file is to be interperted. The character "#!" a.k.a shebang is an indication to the shell that the file is a script and has to be interpreted
+# 2. the next part is to invoke the interpreter and also specify the interpretor type. Interpreter is invoked using the "env" command which resides in the /usr/bin/ directory.
+#    this command spawn a new process of type python which would do the interpretation of the file.
+
+
